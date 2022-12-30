87e1106d3f2348d3d8fc5bc6eddb1c9ac28e7f9f
Juan <projectnoa@users.noreply.github.com>
2022-12-30 16:53:59 -0700

    First Commit

--- SwiftUIColorSample/ContentView.swift
+++ SwiftUIColorSample/ContentView.swift
@@ -11,12 +11,36 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            // Use a system color as the background color of a view
+            Rectangle()
+                .fill(Color.blue)
+                .frame(height: 200)
+                .padding(40)
+            
+            // Use a custom color to create a button
+            Button(action: {
+                print("Button tapped")
+            }) {
+                Text("Custom button")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color(red: 0.5,
+                                      green: 0.2,
+                                      blue: 0.9,
+                                      opacity: 1))
+                    .cornerRadius(8)
+            }
+            
+            // Use a color style to display grayscale text
+            Text("Grayscale text")
+                .foregroundColor(.gray)
+            
+            // Use the .accessibility(label:) modifier to provide a text label for a color
+            Circle()
+                .fill(Color.red)
+                .padding(40)
+                .accessibility(label: Text("Important information"))
         }
-        .padding()
     }
 }
 
